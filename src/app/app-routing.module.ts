4c4bad2a918d1ae863730b80093015c23e74dbe7
SowmyaBetina <sowmya.bs@prodapt.com>
2023-10-14 13:49:24 +0530

    updated code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationComponent } from './location/location.component';
+import { CustomerquoteComponent } from './customerquote/customerquote.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/customerquote', pathMatch: 'full' }, 
+  { path: 'customerquote', component: CustomerquoteComponent },
+  { path: 'location', component: LocationComponent },
+  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerquoteComponent } from './customerquote/customerquote.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LocationComponent } from './location/location.component';
+import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './product/product.component';
+import { FeatureComponent } from './feature/feature.component';
+import { ParameterComponent } from './parameter/parameter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerquoteComponent,
+    LocationComponent,
+    ProductComponent,
+    FeatureComponent,
+    ParameterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
